cee3ab7e9f04f2edd45d7e3a1668e2c3ae6ddf63
Asem Abu Alrub <asem.m.abualrub@gmail.com>
2023-12-18 18:30:34 +0300

    Try navigate overloads & NavOptions

--- navigation-component/src/main/java/com/asemlab/samples/navigation_component/fragments/BusinessFragment.kt
+++ navigation-component/src/main/java/com/asemlab/samples/navigation_component/fragments/BusinessFragment.kt
@@ -1,10 +1,12 @@
 package com.asemlab.samples.navigation_component.fragments
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.fragment.findNavController
 import com.asemlab.samples.navigation_component.R
 import com.asemlab.samples.navigation_component.databinding.FragmentBinding
@@ -21,7 +23,10 @@ class BusinessFragment : Fragment() {
         binding.title.text = "Business"
         binding.t1.text = "Business news"
         binding.t1.setOnClickListener {
-            findNavController().navigate(R.id.action_global_international_details)
+//            findNavController().navigate(R.id.action_global_international_details)
+//            findNavController().navigate(Uri.parse("http://www.example.com/15"))
+            val deepLinkRequest = NavDeepLinkRequest.Builder.fromUri(Uri.parse("http://www.example.com/15")).setAction("HIDE").build()
+            findNavController().navigate(deepLinkRequest)
         }
 
         return binding.root
--- navigation-component/src/main/java/com/asemlab/samples/navigation_component/fragments/news/NewsFragment.kt
+++ navigation-component/src/main/java/com/asemlab/samples/navigation_component/fragments/news/NewsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import com.asemlab.samples.navigation_component.R
 import com.asemlab.samples.navigation_component.databinding.FragmentBinding
 
@@ -20,9 +21,17 @@ class NewsFragment : Fragment() {
 
         binding = FragmentBinding.inflate(inflater, container, false)
         binding.title.text = "News"
-
+        // TODO Add NavOptions when navigate
         binding.newsSections.setOnClickListener {
-            findNavController().navigate(R.id.action_news_nav_to_internationalFragment)
+            findNavController().navigate(
+                R.id.action_news_nav_to_internationalFragment,
+                null,
+                navOptions {
+                    anim {
+                        enter = android.R.animator.fade_in
+                        exit = android.R.animator.fade_out
+                    }
+                })
         }
 
 
