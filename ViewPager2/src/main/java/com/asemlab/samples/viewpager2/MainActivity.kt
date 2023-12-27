e5bd6a6c6d2b0ec219278a614a9c3664026e0bdc
Asem Abu Alrub <asem.m.abualrub@gmail.com>
2023-12-27 15:03:53 +0300

    Add TabLayout with ViewPager2

--- ViewPager2/src/main/java/com/asemlab/samples/viewpager2/MainActivity.kt
+++ ViewPager2/src/main/java/com/asemlab/samples/viewpager2/MainActivity.kt
@@ -3,6 +3,8 @@ package com.asemlab.samples.viewpager2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.asemlab.samples.viewpager2.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,9 +15,15 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
         // TODO Add adapter to ViewPager2
         val adapter = PagerAdapter(this)
         binding.pager.adapter = adapter
+
+//        TODO Attach TabLayout with ViewPager2 & configure it
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab: TabLayout.Tab, i: Int ->
+            tab.text = "Page #$i"
+        }.attach()
+
     }
 }
