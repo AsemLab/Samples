6e1b1fa8f5b2dd9e6b8f52ac3b96e21e21cff7c3
Asem Abu Alrub <asem.m.abualrub@gmail.com>
2024-03-10 15:25:42 +0300

    Add Unit Testing module

---
+++ unit_testing/build.gradle.kts
@@ -0,0 +1,49 @@
+import com.asemlab.samples.Configuration
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin)
+
+}
+
+android {
+    namespace = "com.asemlab.samples.unittesting"
+    compileSdk = Configuration.compileSdk
+
+    defaultConfig {
+        minSdk = Configuration.minSdk
+        targetSdk = Configuration.targetSdk
+        versionCode = Configuration.versionCode
+        versionName = Configuration.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
