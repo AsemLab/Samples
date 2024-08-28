67f026d5cc67928b1f450d0dbfadacc48c15d460
Asem Abu Alrub <asem.m.abualrub@gmail.com>
2024-08-28 18:16:20 +0300

    Add Firebase App Distribution dependencies

--- app_distribution/src/main/java/com/asemlab/samples/appdistribution/MainActivity.kt
+++ app_distribution/src/main/java/com/asemlab/samples/appdistribution/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.asemlab.samples.appdistribution
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.appdistribution.InterruptionLevel
+import com.google.firebase.appdistribution.appDistribution
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +23,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                permissionRequest.launch(Manifest.permission.POST_NOTIFICATIONS)
+            } else {
+                startAppDistributionNotification()
+            }
+        } else {
+            startAppDistributionNotification()
+        }
+    }
+
+    private val permissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            it?.let {
+                if (it)
+                    startAppDistributionNotification()
+            }
+        }
+
+    private fun startAppDistributionNotification() {
+        // TODO Display feedback notification
+        Firebase.appDistribution.showFeedbackNotification(
+            "Click to submit feedback",
+            InterruptionLevel.HIGH
+        )
     }
 }
